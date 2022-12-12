e66df4e96ef211aaa90cbce1693dfebff22c4fda
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2022-12-12 11:15:23 +0000

    2022(day 8): solution for part 2

--- src/advent_of_code/year_2022/day_08/solutions.py
+++ src/advent_of_code/year_2022/day_08/solutions.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 from typing import Optional
 
@@ -33,34 +34,34 @@ def _total_visible_trees(trees_grid: list[list[int]]) -> int:
 
 
 def _trees_right(
-    trees_grid: list[list[int]],
     row_idx: int,
     col_idx: int,
+    trees_grid: list[list[int]],
 ) -> list[int]:
     grid_width: int = len(trees_grid[0])
     return trees_grid[row_idx][col_idx + 1 : grid_width]
 
 
 def _trees_left(
-    trees_grid: list[list[int]],
     row_idx: int,
     col_idx: int,
+    trees_grid: list[list[int]],
 ) -> list[int]:
     return trees_grid[row_idx][0:col_idx]
 
 
 def _trees_above(
-    trees_grid: list[list[int]],
     row_idx: int,
     col_idx: int,
+    trees_grid: list[list[int]],
 ) -> list[int]:
     return [row[col_idx] for row in trees_grid[0:row_idx]]
 
 
 def _trees_below(
-    trees_grid: list[list[int]],
     row_idx: int,
     col_idx: int,
+    trees_grid: list[list[int]],
 ) -> list[int]:
     grid_height: int = len(trees_grid)
     return [row[col_idx] for row in trees_grid[row_idx + 1 : grid_height]]
@@ -73,20 +74,76 @@ def _tree_is_visible(
 ) -> bool:
     current_tree: int = trees_grid[row_idx][col_idx]
     tallest_surrounding_trees: list[int] = [
-        max(_trees_right(trees_grid, row_idx, col_idx)),
-        max(_trees_left(trees_grid, row_idx, col_idx)),
-        max(_trees_above(trees_grid, row_idx, col_idx)),
-        max(_trees_below(trees_grid, row_idx, col_idx)),
+        max(_trees_right(row_idx, col_idx, trees_grid)),
+        max(_trees_left(row_idx, col_idx, trees_grid)),
+        max(_trees_above(row_idx, col_idx, trees_grid)),
+        max(_trees_below(row_idx, col_idx, trees_grid)),
     ]
 
     return any(tree < current_tree for tree in tallest_surrounding_trees)
+
+
+def _all_trees_scenic_scores(trees_grid: list[list[int]]) -> list[int]:
+    grid_width: int = len(trees_grid[0])
+    grid_height: int = len(trees_grid)
+
+    scenic_scores: list[int] = []
+    for row_idx in range(grid_height):
+        for col_idx in range(grid_width):
+            score: int = _tree_scenic_score(row_idx, col_idx, trees_grid)
+            scenic_scores.append(score)
+
+    return scenic_scores
+
+
+def _tree_scenic_score(
+    row_idx: int,
+    col_idx: int,
+    trees_grid: list[list[int]],
+) -> int:
+    current_tree: int = trees_grid[row_idx][col_idx]
+    viewing_distances: list[int] = []
+
+    visible_trees_right: int = 0
+    for tree in _trees_right(row_idx, col_idx, trees_grid):
+        visible_trees_right += 1
+        if tree >= current_tree:
+            break
+    viewing_distances.append(visible_trees_right)
+
+    visible_trees_left: int = 0
+    for tree in reversed(_trees_left(row_idx, col_idx, trees_grid)):
+        visible_trees_left += 1
+        if tree >= current_tree:
+            break
+    viewing_distances.append(visible_trees_left)
+
+    visible_trees_above: int = 0
+    for tree in reversed(_trees_above(row_idx, col_idx, trees_grid)):
+        visible_trees_above += 1
+        if tree >= current_tree:
+            break
+    viewing_distances.append(visible_trees_above)
+
+    visible_trees_below: int = 0
+    for tree in _trees_below(row_idx, col_idx, trees_grid):
+        visible_trees_below += 1
+        if tree >= current_tree:
+            break
+    viewing_distances.append(visible_trees_below)
+
+    return math.prod(viewing_distances)
 
 
 def main() -> None:
     print(PUZZLE_TITLE)
     puzzle_input: str = _load_puzzle_input("input.txt")
     trees_grid = _parse(puzzle_input)
+
     print(f"Answer for part 1: {_total_visible_trees(trees_grid)}")
+
+    scenic_scores = _all_trees_scenic_scores(trees_grid)
+    print(f"Answer for part 2: {max(scenic_scores)}")
 
 
 if __name__ == "__main__":
--- src/advent_of_code/year_2022/day_08/test_solutions.py
+++ src/advent_of_code/year_2022/day_08/test_solutions.py
@@ -1,8 +1,10 @@
 import pytest
 from solutions import (
+    _all_trees_scenic_scores,
     _load_puzzle_input,
     _parse,
     _total_visible_trees,
+    _tree_scenic_score,
     _trees_above,
     _trees_below,
     _trees_left,
@@ -18,23 +20,33 @@ def example_data():
 
 def test_trees_right(example_data):
     row_idx, col_idx = 2, 2
-    assert _trees_right(example_data, row_idx, col_idx) == [3, 2]
+    assert _trees_right(row_idx, col_idx, example_data) == [3, 2]
 
 
 def test_trees_left(example_data):
     row_idx, col_idx = 2, 2
-    assert _trees_left(example_data, row_idx, col_idx) == [6, 5]
+    assert _trees_left(row_idx, col_idx, example_data) == [6, 5]
 
 
 def test_trees_above(example_data):
     row_idx, col_idx = 2, 2
-    assert _trees_above(example_data, row_idx, col_idx) == [3, 5]
+    assert _trees_above(row_idx, col_idx, example_data) == [3, 5]
 
 
 def test_trees_below(example_data):
     row_idx, col_idx = 2, 2
-    assert _trees_below(example_data, row_idx, col_idx) == [5, 3]
+    assert _trees_below(row_idx, col_idx, example_data) == [5, 3]
 
 
 def test_total_visible_trees(example_data):
     assert _total_visible_trees(example_data) == 21
+
+
+def test_trees_scenic_score(example_data):
+    row_idx, col_idx = 1, 2
+    assert _tree_scenic_score(row_idx, col_idx, example_data) == 4
+
+
+def test_all_trees_scenic_scores(example_data):
+    scenic_scores = _all_trees_scenic_scores(example_data)
+    assert max(scenic_scores) == 8
